2139e6cc69b268c84602f2574786bee1564d3896
Robert Kędzior <rob.kedzior@hotmail.com>
2019-10-08 21:03:12 +0200

    adopt-me app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { Router } from "@reach/router";
+import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  const themeHook = useState("darkblue");
+
+  return (
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <h1>Adopt Me!</h1>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useContext } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+import Results from "./Results";
+import ThemeContext from "./ThemeContext";
+
+const SearchParams = () => {
+  const location = "Seattle, WA";
+
+  const [locationText, setLocation] = useState(location);
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+  const [theme, setTheme] = useContext(ThemeContext);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreeds, setBreed]);
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={locationText}
+            onInput={e => setLocation(e.target.value)}
+            placeholder="Location"
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={e => setTheme(e.target.value)}
+            onBlur={e => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="mediumorchid">Medium Orchid</option>
+            <option value="chartreuse">Chartreuse</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
